bf9dd0949c61972822b5699cd15d8e9fdd4debbe
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2024-05-21 20:44:10 -0500

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-
 # -- Path setup --------------------------------------------------------------
 
 import os
@@ -25,7 +23,7 @@ def get_version():
     raise RuntimeError("Unable to find version string.")
 
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
